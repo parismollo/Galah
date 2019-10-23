f1021a137c23b342c83c6e682a1aa75c8f30db43
Paris <parismollochristondis@gmail.com>
2019-10-23 11:13:00 +0200

    while added at main file

--- main.py
+++ main.py
@@ -4,26 +4,27 @@ import gradient_descent
 
 
 def choose_gradient():
-    print("*********************************")
+    print("********************************************")
     print("*******Choose your gradient technique*******")
-    print("*********************************")
+    print("********************************************")
 
-    print("(1) Gradient Descent (2) Mini-batches (3) Stochastic")
+    print("(1): Gradient Descent (2): Mini-batches (3): Stochastic")
+    technique = 0
+    while technique not in range(1, 4):
+        technique = int(input("Which one? "))
 
-    technique = int(input("Which one?"))
+        if technique == 1:
+            print("Starting Gradient Descent....")
+            gradient_descent.run()
+        elif technique == 2:
+            print("Starting Mini-batches....")
+            mini_batches.run()
+        elif technique == 3:
+            print("Starting Stochastic...")
+            stochastic.run()
+        else:
+            print("Choose a valid number")
 
-
-    if technique == 1:
-        print("Starting Gradient Descent....")
-        gradient_descent.run()
-    elif technique == 2:
-        print("Starting Mini-batches....")
-        mini_batches.run()
-    elif technique == 3:
-        print("Starting Stochastic...")
-        stochastic.run()
-    else:
-        print("Something went wrong")
 
 if __name__ == "__main__":
     choose_gradient()
